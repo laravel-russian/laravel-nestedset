ba6be3c46deb226a2469ed02d536d64384d9c882
Matthias Nagel <matthias.h.nagel@posteo.de>
2022-06-21 21:41:07 +0200

    AncestorRelation

--- src/AncestorsRelation.php
+++ src/AncestorsRelation.php
@@ -4,6 +4,10 @@ namespace Kalnoy\Nestedset;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @template TNodeModel of \Illuminate\Database\Eloquent\Model&\Kalnoy\Nestedset\Node
+ * @phpstan-extends BaseRelation<TNodeModel>
+ */
 class AncestorsRelation extends BaseRelation
 {
     /**
@@ -11,7 +15,7 @@ class AncestorsRelation extends BaseRelation
      *
      * @return void
      */
-    public function addConstraints()
+    public function addConstraints(): void
     {
         if ( ! static::$constraints) return;
 
@@ -20,39 +24,39 @@ class AncestorsRelation extends BaseRelation
     }
 
     /**
-     * @param Model $model
-     * @param $related
+     * @param TNodeModel $model
+     * @param TNodeModel $related
      *
      * @return bool
      */
-    protected function matches(Model $model, $related)
+    protected function matches(Model $model, Model $related): bool
     {
         return $related->isAncestorOf($model);
     }
 
     /**
-     * @param QueryBuilder $query
-     * @param Model $model
+     * @param QueryBuilder<TNodeModel> $query
+     * @param TNodeModel $model
      *
      * @return void
      */
-    protected function addEagerConstraint($query, $model)
+    protected function addEagerConstraint(QueryBuilder $query, Model $model): void
     {
         $query->orWhereAncestorOf($model);
     }
 
     /**
-     * @param $hash
-     * @param $table
-     * @param $lft
-     * @param $rgt
+     * @param string $hash
+     * @param string $table
+     * @param string $lft
+     * @param string $rgt
      *
      * @return string
      */
-    protected function relationExistenceCondition($hash, $table, $lft, $rgt)
+    protected function relationExistenceCondition(string $hash, string $table, string $lft, string $rgt): string
     {
         $key = $this->getBaseQuery()->getGrammar()->wrap($this->parent->getKeyName());
 
-        return "{$table}.{$rgt} between {$hash}.{$lft} and {$hash}.{$rgt} and $table.$key <> $hash.$key";
+        return "$table.$rgt between $hash.$lft and $hash.$rgt and $table.$key <> $hash.$key";
     }
 }
