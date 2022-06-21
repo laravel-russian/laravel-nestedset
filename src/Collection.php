fd427347d8be71cc656d0053eea51e30a8c42bf7
Matthias Nagel <matthias.h.nagel@posteo.de>
2022-06-21 21:39:37 +0200

    Collection

--- src/Collection.php
+++ src/Collection.php
@@ -2,38 +2,51 @@
 
 namespace Kalnoy\Nestedset;
 
-use Illuminate\Database\Eloquent\Collection as BaseCollection;
+use Illuminate\Support\Collection as BaseCollection;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Database\Eloquent\Model;
 
-class Collection extends BaseCollection
+/**
+ * @template TNodeModel of \Illuminate\Database\Eloquent\Model&\Kalnoy\Nestedset\Node
+ * @phpstan-extends EloquentCollection<TNodeModel>
+ */
+class Collection extends EloquentCollection
 {
-    /**
+	/**
+	 * @param iterable<TNodeModel> $items
+	 */
+	public final function __construct($items = [])
+	{
+		parent::__construct($items);
+	}
+
+	/**
      * Fill `parent` and `children` relationships for every node in the collection.
      *
      * This will overwrite any previously set relations.
      *
      * @return $this
      */
-    public function linkNodes()
+    public function linkNodes(): static
     {
         if ($this->isEmpty()) return $this;
 
         $groupedNodes = $this->groupBy($this->first()->getParentIdName());
 
-        /** @var NodeTrait|Model $node */
+        /** @var TNodeModel $node */
         foreach ($this->items as $node) {
-            if ( ! $node->getParentId()) {
+            if ($node->getParentId() !== null) {
                 $node->setRelation('parent', null);
             }
 
             $children = $groupedNodes->get($node->getKey(), [ ]);
 
-            /** @var Model|NodeTrait $child */
+            /** @var TNodeModel $child */
             foreach ($children as $child) {
                 $child->setRelation('parent', $node);
             }
 
-            $node->setRelation('children', BaseCollection::make($children));
+            $node->setRelation('children', EloquentCollection::make($children));
         }
 
         return $this;
@@ -46,25 +59,25 @@ class Collection extends BaseCollection
      *
      * If `$root` is provided, the tree will contain only descendants of that node.
      *
-     * @param mixed $root
+     * @param ?TNodeModel $root
      *
-     * @return Collection
+     * @return static
      */
-    public function toTree($root = false)
+    public function toTree(?Node $root = null): static
     {
         if ($this->isEmpty()) {
-            return new static;
+            return new static();
         }
 
         $this->linkNodes();
 
         $items = [ ];
 
-        $root = $this->getRootNodeId($root);
+        $rootId = $this->getRootNodeId($root);
 
-        /** @var Model|NodeTrait $node */
+        /** @var TNodeModel $node */
         foreach ($this->items as $node) {
-            if ($node->getParentId() == $root) {
+            if ($node->getParentId() === $rootId) {
                 $items[] = $node;
             }
         }
@@ -73,25 +86,21 @@ class Collection extends BaseCollection
     }
 
     /**
-     * @param mixed $root
+     * @param ?TNodeModel $root
      *
-     * @return int
+     * @return int|string|null
      */
-    protected function getRootNodeId($root = false)
+    protected function getRootNodeId(?Model $root = null): int|string|null
     {
-        if (NestedSet::isNode($root)) {
+        if ($root !== null) {
             return $root->getKey();
-        }
-
-        if ($root !== false) {
-            return $root;
         }
 
         // If root node is not specified we take parent id of node with
         // least lft value as root node id.
         $leastValue = null;
 
-        /** @var Model|NodeTrait $node */
+        /** @var TNodeModel $node */
         foreach ($this->items as $node) {
             if ($leastValue === null || $node->getLft() < $leastValue) {
                 $leastValue = $node->getLft();
@@ -106,13 +115,13 @@ class Collection extends BaseCollection
      * Build a list of nodes that retain the order that they were pulled from
      * the database.
      *
-     * @param bool $root
+     * @param TNodeModel|null $root
      *
      * @return static
      */
-    public function toFlatTree($root = false)
+    public function toFlatTree(?Model $root = null): static
     {
-        $result = new static;
+        $result = new static();
 
         if ($this->isEmpty()) return $result;
 
@@ -122,14 +131,14 @@ class Collection extends BaseCollection
     }
 
     /**
-     * Flatten a tree into a non recursive array.
+     * Flatten a tree into a non-recursive array.
      *
-     * @param Collection $groupedNodes
-     * @param mixed $parentId
+     * @param BaseCollection<BaseCollection<TNodeModel>> $groupedNodes
+     * @param int|string|null $parentId
      *
      * @return $this
      */
-    protected function flattenTree(self $groupedNodes, $parentId)
+    protected function flattenTree(BaseCollection $groupedNodes, int|string|null $parentId): static
     {
         foreach ($groupedNodes->get($parentId, []) as $node) {
             $this->push($node);
